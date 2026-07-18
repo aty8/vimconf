295a5c4c63d350c3db684a92ee978cc7ddac1231
Andrii Tytarenko <titarenkoan@gmail.com>
2026-07-18 22:58:17 +0300

    Refactor nvim-treesitter configuration for clarity

--- init.lua
+++ init.lua
@@ -46,13 +46,23 @@ require("lazy").setup({
 ------------------------------------------------------------
 {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "cpp", "c", "python", "lua", "bash" },
-      highlight = { enable = true },
+      ensure_installed = {
+        "cpp",
+        "c",
+        "python",
+        "lua",
+        "bash",
+      },
+      highlight = {
+        enable = true,
+      },
     })
-  end
+  end,
 },
 
 ------------------------------------------------------------
